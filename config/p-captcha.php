50091c671ff364e14f5104250ad99c020c66fc32
John Doe <john.doe@gmail.com>
2025-06-22 11:01:54 +0100

    fix false positive + change when to log

--- config/p-captcha.php
+++ config/p-captcha.php
@@ -17,10 +17,10 @@ return [
 
     /**
      * Available challenge types
-     * 
+     *
      * To disable a challenge type, simply remove or comment out the line.
      * The system will automatically use only the enabled types.
-     * 
+     *
      * Available types:
      * - 'beam_alignment': Drag beam source to target (visual)
      * - 'sequence_complete': Complete number sequences (mathematical)
